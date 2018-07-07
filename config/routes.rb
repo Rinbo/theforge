40d3a5c9db2bcfb6c895edbf7a544c7e1646c18a
Rinbo <robin.b@outlook.com>
2018-07-07 14:52:08 +0200

    Add ability to edit user data

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  
+  resources :users
   get '/signup', to: 'users#new'
   root 'static_pages#home'
   get  '/help',    to: 'static_pages#help'
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy' 
-  resources :users  
+  
 end
